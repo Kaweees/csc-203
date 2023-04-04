d776e953465c6ac1e2f40829dc52e5f64c517351
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2023-04-04 01:09:18 -0700

    Lab 0 starter files

---
+++ labs/lab-00/Lab00.java
@@ -0,0 +1,13 @@
+/* this part is as comment similar to comments in Python
+CPE/CSC 203 Lab00
+Name:
+
+Section:
+
+*/
+public class Lab00 {
+   public static void main(String[] arguments) {
+    
+   
+   }
+}
