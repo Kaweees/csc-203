586dbbaeaacedd6a0ed0a75d694a64db14f5346f
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2023-04-17 23:49:33 -0700

    Update Util.java

--- labs/lab-02/part1/Util.java
+++ labs/lab-02/part1/Util.java
@@ -4,24 +4,35 @@ import java.util.Map;
 
 public class Util {
   public static int getYearWithHighestEmissions(Country country) {
+    Map<Integer, Emission> map = country.getEmissions();
     int year = 0;
-    double max = 0;
+    double emissionTotal = 0;
     for (Map.Entry<Integer, Emission> entry : country.getEmissions().entrySet()) {
-      if (entry.getValue().getCO2() > max) {
-        max = entry.getValue().getCO2();
+      Emission emission = entry.getValue();
+      double total = emission.getCH4() + emission.getCO2() + emission.getN2O();
+      if (emissionTotal == 0) {
+        emissionTotal = total;
+        year = entry.getKey();
+      } else if (total > emissionTotal) {
+        emissionTotal = total;
         year = entry.getKey();
       }
     }
     return year;
   }
 
-  public int getYearWithHighestEmissions(Sector sector) {
+  public static int getYearWithHighestEmissions(Sector sector) {
+    Map<Integer, Double> map = sector.getEmissions();
     int year = 0;
-    double max = 0;
-    for (Map.Entry<Integer, Double> entry : sector.getEmissions().entrySet()) {
-      if (entry.getValue() > max) {
-        max = entry.getValue();
-        year = entry.getKey();
+    double emissionHigh = 0;
+    for (Map.Entry<Integer, Double> emission : map.entrySet()) {
+      double total = emission.getValue();
+      if (emissionHigh == 0) {
+        emissionHigh = total;
+        year = emission.getKey();
+      } else if (total > emissionHigh) {
+        emissionHigh = total;
+        year = emission.getKey();
       }
     }
     return year;
