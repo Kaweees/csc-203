8d2cb7eb84206911f342f181d3b9c66fd1e8b748
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2023-04-20 04:48:47 -0700

    Update Viewport.java

--- projects/project-01/src/Viewport.java
+++ projects/project-01/src/Viewport.java
@@ -1,11 +1,44 @@
 public final class Viewport {
-  public int row;
-  public int col;
-  public int numRows;
-  public int numCols;
+  private int row;
+  private int col;
+  private final int numRows;
+  private final int numCols;
 
   public Viewport(int numRows, int numCols) {
     this.numRows = numRows;
     this.numCols = numCols;
+  }
+
+  public int getRow() {
+    return this.row;
+  }
+
+  public int getCol() {
+    return this.col;
+  }
+
+  public int getNumRows() {
+    return this.numRows;
+  }
+
+  public int getNumCols() {
+    return this.numCols;
+  }
+
+  public Point viewportToWorld(int col, int row) {
+    return new Point(col + this.col, row + this.row);
+  }
+
+  public Point worldToViewport(int col, int row) {
+    return new Point(col - this.col, row - this.row);
+  }
+
+  public void shift(int col, int row) {
+    this.col = col;
+    this.row = row;
+  }
+
+  public boolean contains(Point p) {
+    return p.getY() >= this.row && p.getY() < this.row + this.numRows && p.getX() >= this.col && p.getX() < this.col + this.numCols;
   }
 }
