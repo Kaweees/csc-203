5da3c2c676be27d27bf2fc2fc24d895d0856eada
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2023-04-19 21:36:17 -0700

    Update Point.java

--- projects/project-01/src/Point.java
+++ projects/project-01/src/Point.java
@@ -2,26 +2,46 @@
  * A simple class representing a location in 2D space.
  */
 public final class Point {
-  public final int x;
-  public final int y;
+    private final int x;
+    private final int y;
 
-  public Point(int x, int y) {
-    this.x = x;
-    this.y = y;
-  }
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
-  public String toString() {
-    return "(" + x + "," + y + ")";
-  }
+    public int getX() {
+        return this.x;
+    }
 
-  public boolean equals(Object other) {
-    return other instanceof Point && ((Point) other).x == this.x && ((Point) other).y == this.y;
-  }
+    public int getY() {
+        return this.y;
+    }
 
-  public int hashCode() {
-    int result = 17;
-    result = result * 31 + x;
-    result = result * 31 + y;
-    return result;
-  }
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+
+    public boolean equals(Object other) {
+        return other instanceof Point && ((Point) other).getX() == this.x && ((Point) other).getY() == this.y;
+    }
+
+    public int hashCode() {
+        int result = 17;
+        result = result * 31 + this.x;
+        result = result * 31 + this.y;
+        return result;
+    }
+
+    public int distanceSquared(Point p2) {
+        int deltaX = this.x - p2.getX();
+        int deltaY = this.y - p2.getY();
+
+        return deltaX * deltaX + deltaY * deltaY;
+    }
+
+    public boolean adjacent(Point p2) {
+        return (this.x == p2.getX() && Math.abs(this.y - p2.getY()) == 1) || (this.y == p2.getY() && Math.abs(this.x - p2.getX()) == 1);
+    }
+
 }
