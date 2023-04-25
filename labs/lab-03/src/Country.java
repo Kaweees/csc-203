ee2409c9f97aca484ee37c4772b79b8c1061da35
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2023-04-25 00:59:50 -0700

    Lab 3 files

---
+++ labs/lab-03/src/Country.java
@@ -0,0 +1,76 @@
+package src;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Country {
+  private String name;
+  private Map<Integer, Emission> emissions = new HashMap<>();
+
+  public Country(String name, Map<Integer, Emission> emissions) {
+    this.name = name;
+    this.emissions = emissions;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public Map<Integer, Emission> getEmissions() {
+    return this.emissions;
+  }
+
+  public int getYearWithHighestEmissions() {
+    Map<Integer, Emission> map = getEmissions();
+    double emissionHigh = 0;
+    int year = 0;
+    for (Map.Entry<Integer, Emission> emission : map.entrySet()) {
+      Emission emi = emission.getValue();
+      double total = emi.getCO2() + emi.getN2O() + emi.getCH4();
+      if (emissionHigh == 0) {
+        emissionHigh = total;
+        year = emission.getKey();
+      } else {
+        if (total > emissionHigh) {
+          emissionHigh = total;
+          year = emission.getKey();
+        }
+      }
+    }
+    return year;
+  }
+
+  public static Country countryWithHighestCH4InYear(List<Country> countries, int year) {
+    double highEmi = 0;
+    Country highCount = null;
+    for (Country cont : countries) {
+      double emi = cont.getEmissions().get(year).getCH4();
+      if (emi > highEmi) {
+        highEmi = emi;
+        highCount = cont;
+      }
+    }
+    System.out.println(highCount.getName());
+    System.out.println(highEmi);
+    return highCount;
+  }
+
+  public static Country countryWithHighestChangeInEmissions(List<Country> countries, int startYear, int endYear) {
+    double change = 0;
+    Country changeCont = null;
+    for (Country cont : countries) {
+      Map<Integer, Emission> emi = cont.getEmissions();
+      double emiStart = emi.get(startYear).getCO2() + emi.get(startYear).getN2O() + emi.get(startYear).getCH4();
+      double emiEnd = emi.get(endYear).getCO2() + emi.get(endYear).getN2O() + emi.get(endYear).getCH4();
+      double delta = (emiEnd - emiStart);
+      if (delta > change) {
+        change = delta;
+        changeCont = cont;
+      }
+    }
+    System.out.println(changeCont.getName());
+    System.out.println(change);
+    return changeCont;
+  }
+}
---
+++ labs/lab-03/src/Sector.java
@@ -0,0 +1,59 @@
+package src;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Sector {
+  private String name;
+  private Map<Integer, Double> emissions = new HashMap<>();
+
+  public Sector(String name, Map<Integer, Double> emissions) {
+    this.name = name;
+    this.emissions = emissions;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public Map<Integer, Double> getEmissions() {
+    return this.emissions;
+  }
+
+  public int getYearWithHighestEmissions() {
+    Map<Integer, Double> map = getEmissions();
+    int year = 0;
+    double emissionTotal = 0;
+    for (Map.Entry<Integer, Double> emission : map.entrySet()) {
+      double total = emission.getValue();
+      if (emissionTotal == 0) {
+        emissionTotal = total;
+        year = emission.getKey();
+      } else if (total > emissionTotal) {
+        emissionTotal = total;
+        year = emission.getKey();
+      }
+    }
+    return year;
+  }
+
+  public static Sector sectorWithBiggestChangeInEmissions(List<Sector> sectors, int startYear, int endYear) {
+    double finalAvg = -1;
+    Sector avgSect = null;
+    for (Sector sect : sectors) {
+      double sum = 0;
+      for (int i = startYear; i < endYear+1; i++) {
+        sum += sect.getEmissions().get(i);
+      }
+      double avg = (sum) / (endYear - startYear+1);
+      if (avg > finalAvg) {
+        finalAvg = avg;
+        avgSect = sect;
+      }
+    }
+    System.out.println(avgSect.getName());
+    System.out.println(finalAvg);
+    return avgSect;
+  }
+}
