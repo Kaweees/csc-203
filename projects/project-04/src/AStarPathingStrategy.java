ecdc59404f39d6d5df66e8e5d5ed5e51c3ebcee6
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2023-06-02 17:27:00 -0700

    Porject 4

--- projects/project-04/src/AStarPathingStrategy.java
+++ projects/project-04/src/AStarPathingStrategy.java
@@ -16,6 +16,9 @@ class AStarPathingStrategy implements PathingStrategy {
      * neighbors are beside the target set the g, h, f values add them to open
      * list if not in open list add the selected node to close list return path
      */
-    return path;
+    List<Point> closedList = new LinkedList<>();
+    List<Point> openList = new LinkedList<>();
+    openList.add(start);
+
   }
 }
